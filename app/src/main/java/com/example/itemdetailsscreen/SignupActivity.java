aaae16b737e319c8dc2ab1e4e184f855827337c3
Abdallah Abdelaziz <abdallah_taha@aucegypt.edu>
2023-01-19 17:17:39 +0200

    Created Sign Up UI

--- app/src/main/java/com/example/itemdetailsscreen/SignupActivity.java
+++ app/src/main/java/com/example/itemdetailsscreen/SignupActivity.java
@@ -2,7 +2,11 @@ package com.example.itemdetailsscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Button loginBtn = (Button) findViewById(R.id.login_signupAct);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Intent i = new Intent(SignupActivity.this, SigninActivity.class);
+                startActivity (i);
+            }
+        });
+
+        Button signupBtn = (Button) findViewById(R.id.signup_signupAct);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if (CheckAllFields()) {
+                    Toast.makeText(getApplicationContext(), "Trying to sign up!",
+                            Toast.LENGTH_SHORT).show();
+                // }
+            }
+        });
     }
+
+//    private boolean CheckAllFields() {
+//        if (emailEt.length() == 0) {
+//            emailEt.setError("This field is required");
+//            return false;
+//        }
+//        if (passwordEt.length() == 0) {
+//            passwordEt.setError("This field is required");
+//            return false;
+//        }
+//        return true;
+//    }
+
 }
